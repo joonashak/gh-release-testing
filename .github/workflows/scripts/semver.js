11dd08d23492966b4bfe2b92474dc3cbf6591335
Joonas Häkkinen <joonashak@proton.me>
2025-04-22 10:53:41 +0300

    Output match groups from script

--- .github/workflows/scripts/semver.js
+++ .github/workflows/scripts/semver.js
@@ -1,9 +1,13 @@
 module.exports = async ({ context }) => {
+  const { ref } = context;
+
   const re = new RegExp(
     "^refs/tags/v(?<major>[0-9]{1,3})\\.(?<minor>[0-9]{1,3})\\.(?<patch>[0-9]{1,3})(-(?<pre>[0-9A-Za-z\\.-]+))?(\\+(?<build>[0-9A-Za-z\\.-]+))?$"
   );
 
-  if (!re.test(context.ref)) {
+  if (!re.test(ref)) {
     throw Error("Ref does not contain a valid semver tag.");
   }
+
+  return ref.match(re).groups;
 };
